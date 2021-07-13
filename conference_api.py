d963b63a141b7ec4ad5822208a7955cfabc9bbd4
rbowlus <rachel.bowlus@gmail.com>
2021-07-13 08:59:38 -0500

    Partly finished conference api

---
+++ conference_api.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+class Attendees:
+    pass
+
+    def __init__(self, _country, person):
+        self.info = {
+            'country': _country,
+            #length of attendees list
+            'attendeeCount ' : len(self.info['attendees']), 
+            #attendees by email
+            'attendees' : []
+            'StartDate': #the first of two day conference
+        }
